313a912ed79dab2ac0f164dd8a9f5f82bcb95142
Chingiz2727 <k.chyngiz@gmail.com>
2021-07-01 15:39:42 +0600

    Initial Commit

---
+++ AdvCoinApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdvCoinApp
+//
+//  Created by Shyngys Kuandyk on 01.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
